44a99bb1a2ce9294960098c541ee3a694fac5567
benle <benleon@msn.com>
2018-05-24 16:41:22 -0700

    fixed style errors and added getter for endpoints

--- src/edge.py
+++ src/edge.py
@@ -1,8 +1,13 @@
 from src.vertex import Vertex
 
+
 class Edge:
 
-    def __init__(self, end1:Vertex, end2:Vertex):
-        self.endpoints = list()
-        self.endpoints.append(end1)
-        self.endpoints.append(end2)
+    def __init__(self, end1: Vertex, end2: Vertex):
+        self._endpoints = list()
+        self._endpoints.append(end1)
+        self._endpoints.append(end2)
+
+    @property
+    def endpoints(self):
+        return self._endpoints
