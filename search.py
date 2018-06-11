cbb2c9fd628950d1e0a0cdfbb6612c3b6d02aedc
Benjamin Leon <benleon@msn.com>
2018-06-11 14:04:58 -0700

    more GUI trials

--- search.py
+++ search.py
@@ -1,12 +1,33 @@
 from tkinter import *
+from tkinter import ttk
 
 root = Tk("My Searcher App")
+content = ttk.Frame(root, padding=(3,3,12,12))
+graph = ttk.Frame(content)
+
+sel = StringVar()
+
+dsp_cb = ttk.Radiobutton(content, text="DSP", variable=sel, value='d')
+asp_cb = ttk.Radiobutton(content, text="A*", variable=sel, value='a')
+start = ttk.Button(content, text="Start")
 
 node_buttons = list()
 config_buttons = list()
 
-start_button = Button(text="Dijkstras Shortest Path", fg="blue")
-config_buttons.append()
+graph.grid(row=0, column=0, columnspan=3, rowspan=3, sticky=(N, S, E, W))
+content.grid(column=0, row=0, sticky=(N, S, E, W))
+graph.grid(column=0, row=0, columnspan=3, rowspan=2, sticky=(N, S, E, W))
+
+dsp_cb.grid(column=0, row=3)
+asp_cb.grid(column=1, row=3)
+start.grid(column=3, row=3)
+
+root.columnconfigure(0, weight=1)
+root.rowconfigure(0, weight=1)
+graph.columnconfigure(0, weight=3)
+graph.columnconfigure(1, weight=3)
+graph.columnconfigure(2, weight=1)
+
 
 if __name__ == '__main__':
     root.mainloop()
