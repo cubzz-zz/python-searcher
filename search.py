5784ead501772fb0705e293e34e25f6cd54ca995
Benjamin Leon <benleon@msn.com>
2018-06-11 12:54:30 -0700

    setup for creation of GUI

--- search.py
+++ search.py
@@ -2,23 +2,11 @@ from tkinter import *
 
 root = Tk("My Searcher App")
 
-topFrame = Frame(root)
-topFrame.pack()
-bottomFrame = Frame(root)
-bottomFrame.pack(side=BOTTOM)
+node_buttons = list()
+config_buttons = list()
 
-top_buttons = list()
-bot_buttons = list()
-
-top_buttons.append(Label(topFrame, text="Click 1", fg="red", bg="red"))
-top_buttons.append(Label(topFrame, text="Click 2", fg="blue", bg="blue"))
-top_buttons.append(Label(topFrame, text="Click 3", fg="green", bg="green"))
-bot_buttons.append(Label(bottomFrame, text="Click 4", fg="purple"))
-
-for b in top_buttons:
-    b.pack(side=LEFT, fill=X)
-for b in bot_buttons:
-    b.pack(side=BOTTOM)
+start_button = Button(text="Dijkstras Shortest Path", fg="blue")
+config_buttons.append()
 
 if __name__ == '__main__':
     root.mainloop()
